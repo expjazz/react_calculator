a6e13d68fd02a51dc98122da9ef45e89ed8d0c33
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 06:53:10 -0300

    add styling to display and calculator container

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import styled from 'styled-components';
 import Display from './Display';
 import ButtonPannel from './ButtonPannel';
 
+const StyledCalculator = styled.div`
+  width: 700px;
+  margin: auto;
+`;
+
 function App() {
   return (
-    <div id="App">
+    <StyledCalculator>
       <Display />
       <ButtonPannel />
-    </div>
+    </StyledCalculator>
   );
 }
 
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 export default function Display(props) {
   const { result } = props;
+  const StyledDisplay = styled.div`
+    background: gray;
+    height: 100px;
+    p {
+      font-weight: bold;
+      padding: 5px;
+      text-align: right;
+    }
+  `;
   return (
-    <div>
-      {result}
-    </div>
+    <StyledDisplay>
+      <p>
+        {result}
+      </p>
+    </StyledDisplay>
   );
 }
 
