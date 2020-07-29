c94eac0c6b0090de266a0c645203663ee544ab65
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 07:06:59 -0300

    add wide and color props to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 function Button(props) {
-  const { name } = props;
+  const { name, color, wide } = props;
   const StyledButton = styled.button`
-    width: 25%;
-    border: solid 1px black
+    width: ${wide ? '50%' : '25%'};
+    border: solid 1px black;
+    background: ${color};
     p {
       text-align: center;
       color: black;
@@ -24,6 +25,13 @@ function Button(props) {
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
 };
 
 export default Button;
