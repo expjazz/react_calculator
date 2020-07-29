3ba0e0251f63e36a931ae2c4d0f890cf59366ac4
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 05:56:01 -0300

    add buttom panel and display components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Display from './Display';
+import ButtonPannel from './ButtonPannel';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <ButtonPannel/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonPannel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonPannel() {
+  return (
+    <div>
+      Button Pannel
+    </div>
+  )
+}
