8387b42577c3268e17a697c6350dec9156ec3244
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 07:45:20 -0300

    add switch to operate method

---
+++ src/logic/operate.js
@@ -0,0 +1,25 @@
+import Big from 'big.js';
+
+const operate = (one, two, operation) => {
+  let result = new Big(0);
+  const numOne = new Big(one);
+  const numTwo = new Big(two);
+  switch (operation) {
+    case '+':
+      result = numOne.plus(numTwo);
+      break;
+    case '-':
+      result = numOne.minus(numTwo);
+      break;
+    case '*':
+      result = numOne.times(numTwo);
+      break;
+    case '/':
+      result = numOne.dividedBy(numTwo);
+      break;
+    default:
+  }
+  return result;
+};
+
+export default { operate };
