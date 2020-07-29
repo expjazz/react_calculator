443b499cfd8a2553207a1b49671713b927ad0c5b
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 06:34:24 -0300

    initialize brach 3

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Display from './Display';
+import ButtonPannel from './ButtonPannel';
+
+function App() {
+  return (
+    <div id="App">
+      <Display />
+      <ButtonPannel />
+    </div>
+  );
+}
+
+export default App;
