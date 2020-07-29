ada4ffd66f84ed9ceb8762bd34fbaad880d556c1
Expedito Andrade <expeditojazz@gmail.com>
2020-07-29 07:02:08 -0300

    add styling to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 function Button(props) {
   const { name } = props;
+  const StyledButton = styled.button`
+    width: 25%;
+    border: solid 1px black
+    p {
+      text-align: center;
+      color: black;
+    }
+
+  `;
   return (
-    <button type="button">
-      { name }
-    </button>
+    <StyledButton type="button">
+      <p>
+        { name }
+      </p>
+    </StyledButton>
   );
 }
 
