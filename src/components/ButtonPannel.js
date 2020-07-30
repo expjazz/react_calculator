9ebcadfd67c4e37af860b61facddd6ff81c53643
Expedito Andrade <expeditojazz@gmail.com>
2020-07-30 04:35:02 -0300

    add background color props to buttons

--- src/components/ButtonPannel.js
+++ src/components/ButtonPannel.js
@@ -14,32 +14,32 @@ export default function ButtonPannel() {
   return (
     <StyledButtomPanel>
       <div className="row rowOne">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button name="AC" color="#e0e0e0" />
+        <Button name="+/-" color="#e0e0e0" />
+        <Button name="%" color="#e0e0e0" />
         <Button name="÷" />
       </div>
       <div className="row rowTwo">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button name="7" color="#e0e0e0" />
+        <Button name="8" color="#e0e0e0" />
+        <Button name="9" color="#e0e0e0" />
         <Button name="X" />
       </div>
       <div className="row rowThree">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button name="4" color="#e0e0e0" />
+        <Button name="5" color="#e0e0e0" />
+        <Button name="6" color="#e0e0e0" />
         <Button name="-" />
       </div>
       <div className="row rowFour">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+        <Button name="1" color="#e0e0e0" />
+        <Button name="2" color="#e0e0e0" />
+        <Button name="3" color="#e0e0e0" />
         <Button name="+" />
       </div>
       <div className="row rowFive">
-        <Button name="0" wide />
-        <Button name="." />
+        <Button name="0" wide color="#e0e0e0" />
+        <Button name="." color="#e0e0e0" />
         <Button name="=" />
       </div>
     </StyledButtomPanel>
