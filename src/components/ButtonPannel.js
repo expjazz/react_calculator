212c16edc893337f4968a230c2afda9fd0249a01
Expedito Andrade <expeditojazz@gmail.com>
2020-07-30 11:00:07 -0300

    fix button color

--- src/components/ButtonPannel.js
+++ src/components/ButtonPannel.js
@@ -15,32 +15,32 @@ export default function ButtonPannel(props) {
   return (
     <StyledButtomPanel>
       <div className="row rowOne">
-        <Button handleClick={handleClick} name="AC" />
-        <Button handleClick={handleClick} name="+/-" />
-        <Button handleClick={handleClick} name="%" />
+        <Button handleClick={handleClick} name="AC" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="+/-" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="%" color="#e0e0e0" />
         <Button handleClick={handleClick} name="÷" />
       </div>
       <div className="row rowTwo">
-        <Button handleClick={handleClick} name="7" />
-        <Button handleClick={handleClick} name="8" />
-        <Button handleClick={handleClick} name="9" />
+        <Button handleClick={handleClick} name="7" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="8" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="9" color="#e0e0e0" />
         <Button handleClick={handleClick} name="X" />
       </div>
       <div className="row rowThree">
-        <Button handleClick={handleClick} name="4" />
-        <Button handleClick={handleClick} name="5" />
-        <Button handleClick={handleClick} name="6" />
+        <Button handleClick={handleClick} name="4" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="5" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="6" color="#e0e0e0" />
         <Button handleClick={handleClick} name="-" />
       </div>
       <div className="row rowFour">
-        <Button handleClick={handleClick} name="1" />
-        <Button handleClick={handleClick} name="2" />
-        <Button handleClick={handleClick} name="3" />
+        <Button handleClick={handleClick} name="1" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="2" color="#e0e0e0" />
+        <Button handleClick={handleClick} name="3" color="#e0e0e0" />
         <Button handleClick={handleClick} name="+" />
       </div>
       <div className="row rowFive">
-        <Button handleClick={handleClick} name="0" wide />
-        <Button handleClick={handleClick} name="." />
+        <Button handleClick={handleClick} name="0" color="#e0e0e0" wide />
+        <Button handleClick={handleClick} name="." color="#e0e0e0" />
         <Button handleClick={handleClick} name="=" />
       </div>
     </StyledButtomPanel>
